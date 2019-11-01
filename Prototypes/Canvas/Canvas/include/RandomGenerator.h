3ae41db536812d47b799225f26a22cb1cce6cc01
mequint <katarn2000@hotmail.com>
2019-11-01 13:11:34 -0700

    Prototype for an SFML Image based canvas that performs per pixel operations such as pixel drawing, line drawing, rectangle drawing, and circle drawing

---
+++ Prototypes/Canvas/Canvas/include/RandomGenerator.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SFML/System/Mutex.hpp>
+#include <random>
+
+class RandomGenerator {
+public:
+	RandomGenerator();
+
+	int Generate(int min, int max);
+	float Generate(float min, float max);
+
+	int operator()(int min, int max);
+	float operator()(float min, float max);
+
+private:
+	std::random_device m_device;
+	std::mt19937 m_engine;
+	std::uniform_int_distribution<int> m_intDistribution;
+	std::uniform_real_distribution<float> m_floatDistribution;
+
+	sf::Mutex m_mutex;
+};
